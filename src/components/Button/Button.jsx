2f8fb49e26d1e51395aa6f91540177aa6d62ad81
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-12-25 14:53:58 +0100

    classe ajouté , fichier Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,9 @@
 function Button({ text, onClick }) {
-  return <button onClick={onClick}>{text}</button>;
+  return (
+    <button className="button" onClick={onClick}>
+      {text}
+    </button>
+  );
 }
 
 export default Button;
